745fe3681ee6edefe02b810d27fef35c36a11d46
christopher rehm <christopherrehm@web.de>
2023-10-13 15:36:54 +0200

    oops

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Tech Friday!",
-  description: "program an registration",
+  description: "program, registration",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
