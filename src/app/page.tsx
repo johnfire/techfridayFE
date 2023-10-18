b6a41c37f8a8ad1d3bb50aacaca8bb09390c16a1
christopher rehm <christopherrehm@web.de>
2023-10-18 08:53:33 +0200

    try removing import and using locally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,14 @@ import RoomHeader from "./components/roomHeader";
 import Header from "./components/header";
 import RoomColumn from "./components/roomColumn";
 import { talk } from "@/interfaces/interfaces";
-import { rooms, BUTTON_STYLE, BASIS_URL } from "@/constants";
+import { rooms, BASIS_URL } from "@/constants";
 
 const TARENT_SYMBOL_SIZE: number = 200;
+const BUTTON_STYLE = "border-2 bg-blue-900 items-center text-white px-5 mx-4";
+const BUTTON_STYLE_TIGHT = "border-2 bg-blue-900 items-center text-white";
+const TEXT_BOLD = "text-lg font-semibold";
+const SMALL_BORDER = "border-2 border-black border-solid  mb-5";
+const MEDIUM_BORDER = "border-4 border-black border-solid";
 
 // trying to find the problem here ..
 
