1a45d72436bf833bae07d96d887b48643771843a
christopher rehm <christopherrehm@web.de>
2023-10-20 14:48:17 +0200

    fornatting

--- src/app/components/roomHeader.tsx
+++ src/app/components/roomHeader.tsx
@@ -4,7 +4,6 @@ const RoomHeader = () => {
   return (
     <div className="flex flex-row items-center justify-around bg-amber-100 border-2 border-blue-900 m-3 rounded-md text-2xl font-extrabold text-orange-600">
       <p>Townhall 1</p>
-      <p>Townhall 2 </p>
       <p>Blauer Affe</p>
       <p>N8schicht</p>
       <p>Auzbi-Raum</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,10 +124,8 @@ const MainDisplayPage = () => {
           <RoomHeader />
         </div>
 
-        <p>test for here</p>
-
         <div className="container w-full m-auto grid grid-cols-4 grid-rows-layout place-items-stretch justify-between gap-3">
-          <div className="bg-amber-600 col-span-4 items-center "> introduction</div>
+          <div className="bg-amber-600 col-span-4 justify-center "> introduction</div>
           <div className="bg-pink-300 w-full h-full">2</div>
           <div className="bg-blue-300 w-full h-full">3</div>
           <div className="bg-pink-300 w-full h-full">4</div>
