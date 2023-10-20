704a25d82ba03c5ae61103402b580e6ac6dbfabe
christopher rehm <christopherrehm@web.de>
2023-10-20 14:24:05 +0200

    col sp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -125,12 +125,12 @@ const MainDisplayPage = () => {
         </div>
 
         <div className="container w-full m-auto grid grid-cols-4 grid-rows-13 place-items-stretch justify-between gap-3">
-          <div className="bg-blue-300 col-span-4">1</div>
+          <div className="bg-blue-300 col-span-4 ">1</div>
           <div className="bg-pink-300 w-full h-full">2</div>
           <div className="bg-blue-300 w-full h-full">3</div>
           <div className="bg-pink-300 w-full h-full">4</div>
           <div className="bg-blue-300 w-full h-full">5</div>
-          <div className="bg-pink-300 w-full h-full">6</div>
+          <div className="bg-pink-300 col-span-4 w-full h-full">6</div>
           <div className="bg-blue-300 w-full h-full">7</div>
           <div className="bg-pink-300 w-full h-full">8</div>
           <div className="bg-blue-300 w-full h-full">9</div>
