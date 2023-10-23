cb234d0045628ab2e4280e91f1ff4a96b1e25a65
christopher rehm <christopherrehm@web.de>
2023-10-23 09:38:58 +0200

    ok

--- src/app/components/talkComponent.tsx
+++ src/app/components/talkComponent.tsx
@@ -86,7 +86,7 @@ const TalkComponent = ({
   };
   return (
     <div
-      className="bg-white rounded-md flex justify-center items-center m-2 border-2 border-black border-solid "
+      className="bg-white rounded-md flex justify-center items-center m-2 border-2 border-black border-solid  oveflow-hidden"
       style={{
         marginBottom: "4px",
         padding: "5px",
