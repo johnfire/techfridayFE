c3199c4c117a9c0805a002a7dbeb917f18f6f36e
christopher rehm <christopherrehm@web.de>
2023-10-31 16:20:39 +0100

    spacing

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,9 +5,12 @@ const Footer = () => {
     <div className="flex flex-row justify-between w-full">
       <p>© 2023 Qvest Digital AG. All rights reserved</p>
       <div className="flex flex-row justify-end w-full">
-        <a href="https://www.tarent.de/presse">presse</a>
+        <a href="https://www.tarent.de/presse">presse </a>
+        <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
         <a href="https://www.tarent.de/impressum">impressum</a>
+        <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
         <a href="https://www.tarent.de/datenschutz">datenschutz</a>
+        <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
       </div>
     </div>
   );
